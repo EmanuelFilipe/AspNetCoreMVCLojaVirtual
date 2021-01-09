a82cc0ecc135b743c8c38aaffcb06afee39252ca
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-01-09 16:50:13 -0300

    subindo primeiros arquivos

---
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            Contato contato = new Contato
+            {
+                Nome = HttpContext.Request.Form["nome"],
+                Email = HttpContext.Request.Form["email"],
+                Texto = HttpContext.Request.Form["texto"]
+            };
+
+            return new ContentResult() { Content = $"Dados recebidos com sucesso!<br /> Nome: {contato.Nome} <br />Email: {contato.Email} <br />Texto: {contato.Texto}", ContentType = "text/html" };
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ LojaVirtual/LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,26 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            // servidor que vai enviar a mensagem
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("cloudwalstorn@gmail.com", "senha");
+            smtp.EnableSsl = true;
+
+            //MailMessage = constroi a mensagem a ser enviada
+            MailMessage mensagem = new MailMessage();
+
+        }
+    }
+}
