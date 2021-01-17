ad05f1e2dd3e8a51ccfc41359d3799aa2f69b7c6
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2021-01-17 16:32:23 -0300

    Parte 9 - Configuração de banco de dados e Migration

---
+++ LojaVirtual/LojaVirtual/Database/LojaVirtualContext.cs
@@ -0,0 +1,24 @@
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.DataBase
+{
+    public class LojaVirtualContext : DbContext
+    {
+        /*
+         * EF Core - ORM
+         * ORM -> Biblioteca que mapea objetos para banco de dados relacionais
+         */
+
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
